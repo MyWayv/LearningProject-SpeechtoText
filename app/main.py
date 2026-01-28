2e36aecef50533e56ca581b6a8ca643769985c9a
ErikCompa <erikc@mywayv.com>
2026-01-28 14:41:17 -0500

    removed CORS since not needed anymore

--- app/main.py
+++ app/main.py
@@ -5,26 +5,12 @@ load_dotenv(".env")
 from pathlib import Path
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from app.routes import http_router, ws_router
 
 # FastAPI app
 app = FastAPI()
-
-# CORS setup
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # Include routers
 app.include_router(http_router)
